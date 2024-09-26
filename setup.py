060839274b113ac8001bbc0594420ceebb090687
Ali Fallahi <53713455+a-fa@users.noreply.github.com>
2024-09-26 16:44:13 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,19 @@ import numpy as np
 from Cython.Distutils import build_ext
 
 ext_modules=[
-    Extension("PythIon.Parsers.cparsers",["PythIon/Parsers/cparsers.pyx"],include_dirs=[np.get_include()] )
+    Extension("ioniq.cparsers",["src/cparsers.pyx"],include_dirs=[np.get_include()] )
 ]
 
 setup(
-    name='PythIon',
-    version='0.3.0',
+    name='ioniq',
+    version='0.1',
     author='Ali Fallahi',
     author_email='fallahi.a@northeastern.edu',
-    packages=['PythIon'],
-    description='Nanopore Data Analysis package.',
-    requires=['cython'],
-    include=["PythIon*"],
+    url='https://github.com/wanunulab/ioniq/',
+    packages=['ioniq'],
+    description='A modular nanopore data analysis package.',
+    requires=[line.strip() for line in open('requirements.txt')],
+    include=["src*"],
     cmdclass={"build_ext": build_ext},
     ext_modules=ext_modules
     
