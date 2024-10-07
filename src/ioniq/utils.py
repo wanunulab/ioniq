dd4a3fa6a05ebb39c013a31a65058769c5017821
dinboyko <boyko.d@northeastern.edu>
2024-10-07 15:19:36 -0400

    added SiEval to utils, removed wheel and pip from reqs.txt

--- src/ioniq/utils.py
+++ src/ioniq/utils.py
@@ -75,3 +75,88 @@ def split_voltage_steps(voltage: np.ndarray, n_remove=0, as_tuples=False):
         return start_indices, end_indices
     # Optional: return list of tuples of start and end
     return [(start_ind, end_ind) for start_ind, end_ind in zip(start_indices, end_indices)]
+
+
+def si_eval(value, unit=None, return_unit=False):
+    """
+    Convert a value with SI prefix to its equivalent value.
+
+    If the `value` is a string, it splits the numeric value and the unit and
+    applies SI prefix multiplier.
+
+    If the value is already numeric, it directly applies SI prefix multiplier.
+
+    :param value: The value to be converted.
+    :type value: str or float
+    :param unit: The unit type, default=None
+    :type unit: str, optional
+    :param return_unit: If True, the function returns a tuple with the numeric value and the unit.
+    :type return_unit: bool, optional
+    :return: The converted value, optionally with the unit if `return_unit` is True.
+    :rtype: float or tuple
+
+    """
+    # If value is a string, split it to separate the value from unit
+    if isinstance(value, str):
+        try:
+            numeric_val, unit_full = value.strip().split()
+            numeric_val = float(numeric_val)
+            final_value = numeric_val * _si_multiplier_unit(unit_full[0])
+        except ValueError:
+            raise ValueError(f"Invalid value format: {value}. Should be 'number unit'(Ex:'1.2 kHz')")
+
+    # If value is a numer:
+    elif isinstance(value, (int, float)):
+        if unit is None:
+            raise ValueError("Unit is not provided. Ex: 1.2, 'kHz'")
+        final_value = value * _si_multiplier_unit(unit[0])
+    else:
+        raise TypeError("Provide the parameters!")
+    if return_unit:
+        return final_value, unit[1:]
+    return final_value
+
+
+def _get_prefix_val() -> dict:
+    """
+    Return a dictionary of SI prefixes and the corresponding multiplier values.
+
+    This function is used internally to provide the SI prefix values.
+
+    :return: A dictionary mapping SI prefixes to their values.
+    :rtype: dict
+    """
+
+    prefix = {
+    "f": 1e-15,
+    "p": 1e-12,
+    "n": 1e-9,
+    "u": 1e-6,
+    "μ": 1e-6,
+    "m": 1e-3,
+    "c": 1e-2,
+    "k": 1e3,
+    "M": 1e6,
+    "G": 1e9,
+    "T": 1e12,
+    }
+    return prefix
+
+
+def _si_multiplier_unit(unitstr: str) -> float:
+    """
+    Return a multiplier for a given unit prefix.
+
+    Given a single character string (SI prefix) it
+    returns the corresponding multiplier value.
+
+    :param unitstr: A single character string = SI prefix.
+    :type unitstr: str
+    :return: The multiplier corresponding to the given SI prefix.
+    :rtype: float
+    """
+
+    if unitstr in _get_prefix_val():
+        return _get_prefix_val()[unitstr]
+    else:
+        raise ValueError(f"Unit prefix is not known: {unitstr}, see available:\n{_get_prefix_val()}")
