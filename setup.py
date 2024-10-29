7ac891e35ae011a32dd4afc665832368c242be68
dinboyko <boyko.d@northeastern.edu>
2024-10-29 09:16:32 -0400

    update pip install . with pyproject.toml, removed requirements.txt

--- setup.py
+++ setup.py
@@ -1,25 +1,12 @@
-from setuptools import setup
-from setuptools.extension import Extension
+from setuptools import setup, Extension
+from Cython.Build import cythonize
 import numpy as np
-from Cython.Distutils import build_ext
 
-ext_modules=[
-    Extension("ioniq.cparsers",["src/ioniq/cparsers.pyx"],include_dirs=[np.get_include()] )
+extensions = [
+    Extension(
+        "ioniq.cparsers", ["src/ioniq/cparsers.pyx"], include_dirs=[np.get_include()])
 ]
 
 setup(
-    name='ioniq',
-    version='0.1',
-    author='Ali Fallahi',
-    author_email='fallahi.a@northeastern.edu',
-    url='https://github.com/wanunulab/ioniq/',
-    packages=['ioniq'],
-    package_dir={'':'src/'},
-    description='A modular nanopore data analysis package.',
-    requires=['cython'],
-    include=["src/ioniq*"],
-    cmdclass={"build_ext": build_ext},
-    ext_modules=ext_modules
-    
-    
+    ext_modules=cythonize(extensions)
 )
