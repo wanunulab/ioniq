9810a38ae0c2d374666f460583d64be17c14dfeb
Ali Fallahi <53713455+a-fa@users.noreply.github.com>
2025-09-23 14:54:57 -0400

    Create plotting.py
    placeholder functions for simple plotting

---
+++ src/ionique/plotting.py
@@ -0,0 +1,30 @@
+"""
+Module ionique.plotting
+Includes convenience functions for generating routine plots.
+"""
+
+def qp_trace(...):
+  """ 
+  quickly plot a trace, or segment.
+  """
+  
+  pass
+
+def qp_scatter(...):
+  """
+  quickly generate a scatterplot from specified parameters. wrapper for seaborn scatterplot
+  """
+  pass
+
+def qp_histogram(...):
+  """
+  quickly plot a 1-D histogram from specified parameter. wrapper for seaborn histplot
+  """
+  pass
+
+def _get_feature_df_from_segments(...):
+  """
+  if a qp (non-trace) function receives a segment, extract required features and return a pandas dataframe.
+  """
+  pass
+def 
