c8445a3c0e06506d2f00078f439b60fb074a4f7f
Ali Fallahi <53713455+a-fa@users.noreply.github.com>
2025-09-26 10:21:40 -0400

    convenience functions for the unsavvy

--- src/ionique/plotting.py
+++ src/ionique/plotting.py
@@ -1,30 +1,30 @@
+#!/usr/bin/env python
 """
 Module ionique.plotting
 Includes convenience functions for generating routine plots.
 """
 
-def qp_trace(...):
+def qp_trace(**args):
   """ 
   quickly plot a trace, or segment.
   """
   
   pass
 
-def qp_scatter(...):
+def qp_scatter(**args):
   """
   quickly generate a scatterplot from specified parameters. wrapper for seaborn scatterplot
   """
   pass
 
-def qp_histogram(...):
+def qp_histogram(**args):
   """
   quickly plot a 1-D histogram from specified parameter. wrapper for seaborn histplot
   """
   pass
 
-def _get_feature_df_from_segments(...):
+def _get_feature_df_from_segments(**args):
   """
   if a qp (non-trace) function receives a segment, extract required features and return a pandas dataframe.
   """
   pass
-def 
