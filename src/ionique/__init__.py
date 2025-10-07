56cfe67571f2d7513473a0bdb0774e6fb2e6d953
Ali Fallahi <53713455+a-fa@users.noreply.github.com>
2025-10-07 13:52:08 -0400

    Update __init__.py

--- src/ionique/__init__.py
+++ src/ionique/__init__.py
@@ -5,7 +5,8 @@ import ionique.io as io
 import ionique.utils as utils
 import ionique.parsers as parsers
 import ionique.plotting as plotting
+import ionique.simple as simple
 
-__all__=["core","datatypes","io","utils","parsers","plotting"]
+__all__=["core","datatypes","io","utils","parsers","plotting","simple"]
 def __dir__():
     return sorted(list(set(list(globals().keys())+__all__)))
